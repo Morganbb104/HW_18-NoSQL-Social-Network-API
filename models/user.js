729e597f9c9c03c6b8c51264bf36f1027b3d2500
CHENG CHIEN <hcc780921@gmail.com>
2022-04-25 16:52:10 -0700

    User.js completed

--- models/user.js
+++ models/user.js
@@ -1,1 +1,43 @@
-const { Schema, model } = require("mongoose");
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Your email was Wrong,please enter a valid email address",]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
+
+// Create the model user using the userSchema
+const User = model("User", userSchema);
+module.exports = User;
