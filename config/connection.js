c270d24ea54428dc3138a01e4a703519b716a614
CHENG CHIEN <hcc780921@gmail.com>
2022-04-25 18:49:12 -0700

    routes completed

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect('mongodb://localhost:27017/mygroceryDB', {
+  useFindAndModify: false,  
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
---
+++ controller/thoughtController.js
@@ -0,0 +1,2 @@
+const { User } = require("../models");
+const { populate } = require("../models/User");
