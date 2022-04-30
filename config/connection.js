05ad8838e3015a53223ad8da38a79694d955a292
CHENG CHIEN <hcc780921@gmail.com>
2022-04-30 13:22:52 -0700

    error// useFindAndModify: false fixed

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://localhost:27017/mygroceryDB', {
-  useFindAndModify: false,  
+mongoose.connect('mongodb://localhost:27017/userThoughtDB', {
+  // useFindAndModify: false,  
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
